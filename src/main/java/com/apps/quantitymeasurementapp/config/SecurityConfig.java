67c5cae1b27210e239b05930c010c3fcb4876433
unknown <arghadeepnath28@gmail.com>
2026-05-22 10:22:27 +0530

    UC18 Done

--- src/main/java/com/apps/quantitymeasurementapp/config/SecurityConfig.java
+++ src/main/java/com/apps/quantitymeasurementapp/config/SecurityConfig.java
@@ -6,10 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${app.jwt.secret}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -18,12 +26,44 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().permitAll()
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/oauth2/**",
+                                "/login/**",
+                                "/error"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(form -> form.disable());
+
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl(
+                                "/api/auth/success",
+                                true
+                        )
+                )
+
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(Customizer.withDefaults())
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        SecretKeySpec key =
+                new SecretKeySpec(
+                        secretKey.getBytes(),
+                        "HmacSHA256"
+                );
+
+        return NimbusJwtDecoder
+                .withSecretKey(key)
+                .build();
     }
 }
